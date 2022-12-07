de25b4a896cca23abb757ca4261a80e93e89f904
Viktoria-95 <119070943+Viktoria-95@users.noreply.github.com>
2022-12-07 23:20:55 +0300

    21-ая попытка

--- Zadanie_13/Program.cs
+++ Zadanie_13/Program.cs
@@ -1,4 +1,43 @@
-﻿// Задача 21
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-// A (3,6,8); B (2,1,-7), -> 15.84
-// A (7,-5, 0); B (1,-1,9) -> 11.53
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// Console.WriteLine("Введите первое число ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+int n = ReadInt("Введите число: ");
+int len = NumberLen(n);
+SumNumbers(n, len);
+
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine(sum);
+}
