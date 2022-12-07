bd5ff264164d5f76380a83f25c8b5bf04c2e1d16
Viktoria-95 <119070943+Viktoria-95@users.noreply.github.com>
2022-12-07 22:32:43 +0300

    19-ая попытка

--- Zadanie_11/Program.cs
+++ Zadanie_11/Program.cs
@@ -3,41 +3,10 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.WriteLine("Введите число: ");
+uint N = Convert.ToUInt32(Console.ReadLine()); // uint - тип данных который выводит положительные целые числа
 
-// Задача 21
-// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
-// A (3,6,8); B (2,1,-7), -> 15.84
-// A (7,-5, 0); B (1,-1,9) -> 11.53
-
-
-class GFG //присваиваем класс программы, для возможности использования статического члена - distance
-{ // public static void Main - назначаем модификатор, тип и имя функции
-    public static void Main() 
-{ // считываем вводимые числа с консоли и присваиваем тип float
-     Console.Write("Введите координату x1: ");
-     float x1 = float.Parse(Console.ReadLine()); // float - тип данных, float.Parse - конвертация данных
-     Console.Write("Введите координату y1: ");
-     float y1 = float.Parse(Console.ReadLine());
-     Console.Write("Введите координату z1: ");
-     float z1 = float.Parse(Console.ReadLine());
-     Console.Write("Введите координату x2: ");
-     float x2 = float.Parse(Console.ReadLine());
-     Console.Write("Введите координату y2: ");
-     float y2 = float.Parse(Console.ReadLine());
-     Console.Write("Введите координату z2: ");
-     float z2 = float.Parse(Console.ReadLine());
-    distance(x1, y1, z1,
-    x2, y2, z2);
-}
-// static void distance - назначаем модификатор, тип и имя функции
-static void distance(float x1, float y1,
-                    float z1, float x2,
-                    float y2, float z2)
-{ //расписываем формулу нахождения растояния между координатами в 3D пространстве
-    double d = Math.Pow((Math.Pow(x2 - x1, 2) +
-                        Math.Pow(y2 - y1, 2) +
-                        Math.Pow(z2 - z1, 2) *
-                        1.0), 0.5);
-    Console.WriteLine($"Расстояние между 3 точками в 3D пространстве {d}");
-    return;
+for (int i = 1; i <= N; i++) //
+{
+    Console.Write($" {i*i*i} "); //
 }
