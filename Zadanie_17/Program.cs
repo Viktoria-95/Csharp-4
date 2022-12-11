ac443a64a71be5a8e81912cf1ea5924565f5d3f6
Viktoria-95 <119070943+Viktoria-95@users.noreply.github.com>
2022-12-11 19:49:56 +0300

    38-ая попытка

--- Zadanie_17/Program.cs
+++ Zadanie_17/Program.cs
@@ -10,48 +10,92 @@ switch(n)
         break;
     case 2:
         Task2();
-        break;   
+        break;
     default:
         break;
 }
 
 void Task1()
 {
-Console.Write("Введите элементы(через пробел): ");
-int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int count = 0;
-
-for (int i = 0; i < arr.Length; i++)
+Console.Write("Введите числа через запятую: ");
+int[] numbers = StringToNum(Console.ReadLine());
+PrintArray(numbers);
+int sum = 0;
+for (int i = 0; i < numbers.Length; i++)
 {
-    if (arr[i] > 0)
+    if (numbers[i] > 0)
     {
-        count++;
+        sum++;
     }
 }
+Console.WriteLine();
+Console.WriteLine($"количество значений больше 0 = {sum}");
 
-Console.WriteLine($"Кол-во элементов > 0: {count}");
+
+int[] StringToNum(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    int[] numbers = new int [count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input [i] != ',')
+        {
+        if(i != input.Length - 1)
+        {
+            temp += input [i].ToString();
+            i++;
+        }
+        else
+        {
+            temp += input [i].ToString();
+            break;
+        }
+        }
+        numbers[index] = Convert.ToInt32(temp);
+        index++;
+    }
+    return numbers;
+}
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write("]");
+}
 }
 
 void Task2()
 {
-Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
-Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
 
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
 
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
-
-x = Math.Round(x, 3);
-y = Math.Round(y, 3);
-
-Console.WriteLine($"Пересечение в точке: ({x};{y})");
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
 }
+
 
 
 // Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
